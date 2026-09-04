c6753d84439cbf03602aa579f507cf7c5172ac55
Joachim Eriksson <sa0azy@gmail.com>
2026-09-04 10:10:23 +0200

    feat(fitness): add Fitness configuration constants

--- custom_components/garmin_connect/const.py
+++ custom_components/garmin_connect/const.py
@@ -3,6 +3,7 @@
 from typing import Final
 
 DOMAIN: Final = "garmin_connect"
+FITNESS_DATA_KEY: Final = f"{DOMAIN}_fitness"
 
 # Config entry keys
 CONF_TOKEN: Final = "token"
@@ -12,6 +13,12 @@ CONF_CLIENT_ID: Final = "client_id"
 # Options
 CONF_IS_CN: Final = "is_cn"
 CONF_SCAN_INTERVAL: Final = "scan_interval"
+CONF_FITNESS_MAX_HR: Final = "fitness_max_hr"
+CONF_FITNESS_SEX: Final = "fitness_sex"
 DEFAULT_SCAN_INTERVAL: Final = 300  # 5 minutes
 MIN_SCAN_INTERVAL: Final = 60  # 1 minute
 MAX_SCAN_INTERVAL: Final = 3600  # 1 hour
+FITNESS_HISTORY_DAYS: Final = 90
+FITNESS_MAX_HR_MIN: Final = 100
+FITNESS_MAX_HR_MAX: Final = 250
+FITNESS_SEX_OPTIONS: Final = ("male", "female")
